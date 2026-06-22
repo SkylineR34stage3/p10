1bf24e8b9efe198f4df5457c59336dd755ab2924
Artem Sokolov <arsokolo@1-E-11.42heilbronn.de>
2026-06-22 18:48:24 +0200

    ex1: added power amplifier + changed logic to return lambdas

--- ex1/higher_magic.py
+++ ex1/higher_magic.py
@@ -10,16 +10,28 @@ def fireball(target: str, power: int) -> str:
 
 
 def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
-    def combined(target: str, power: int) -> tuple:
-        return (spell1(target, power), spell2(target, power))
-    return combined
+    return lambda target, power: (spell1(target, power), spell2(target, power))
+    # def combined(target: str, power: int) -> tuple:
+    #     return (spell1(target, power), spell2(target, power))
+    # return combined
 
 
-def main():
+def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
+    return lambda target, power: (base_spell(target, power * multiplier))
+    # def amplified(target: str, power: int) -> str:
+    #     return base_spell(target, power * multiplier)
+    # return amplified
+
+
+def main() -> None:
     print("Testing spell combiner...")
     combined = spell_combiner(heal, fireball)
     res1, res2 = combined("Dragon", 10)
     print(res1, res2, sep="\n")
+
+    print("\nTesting power amplifier...")
+    mega_fireball = power_amplifier(fireball, 3)
+    print(mega_fireball("Dragon", 10))
 
 
 if __name__ == "__main__":
