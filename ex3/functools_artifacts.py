11d83d558500b2bb20a1cda038359d5093a266cc
Artem Sokolov <arsokolo@1-E-11.42heilbronn.de>
2026-06-24 18:31:10 +0200

    ex3: added partial enchantment

--- ex3/functools_artifacts.py
+++ ex3/functools_artifacts.py
@@ -1,5 +1,6 @@
-from functools import reduce
+from functools import reduce, partial
 from operator import add, mul
+from collections.abc import Callable
 
 
 class InvalidOperation(Exception):
@@ -21,6 +22,14 @@ def spell_reducer(spells: list[int], operation: str) -> int:
     return reduce(ops[operation], spells)
 
 
+def partial_enchanter(base_enchantment: Callable) -> dict[str, Callable]:
+    return {
+        "fire": partial(base_enchantment, 50, "Fire"),
+        "ice": partial(base_enchantment, 50, "Ice"),
+        "skibidi": partial(base_enchantment, 50, "Skibidi"),
+    }
+
+
 def test_spell_reducer() -> None:
     print("Testing spell reducer...")
     spell_powers = [23, 35, 33, 32, 32, 40]
@@ -30,8 +39,21 @@ def test_spell_reducer() -> None:
               spell_reducer(spell_powers, op))
 
 
+def test_partial_enchanter() -> None:
+    print("\nTesting partial enchanter...")
+    enchantments = partial_enchanter(
+        lambda s, e, x: f"{e} enchantment (power {s}) applied to {x}"
+    )
+
+    print(enchantments["fire"]("Sword"))
+    print(enchantments["ice"]("Sword"))
+    print(enchantments["skibidi"]("Tung Tung Sahur"))
+
+
 def main() -> None:
     test_spell_reducer()
+
+    test_partial_enchanter()
 
 
 if __name__ == "__main__":
